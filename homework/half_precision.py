72db76f955a2a4a986a5a0838830d4f20d34e332
root <Brian McGee>
2026-09-12 22:31:10 +0000

    Completed half precision

--- homework/half_precision.py
+++ homework/half_precision.py
@@ -43,11 +43,11 @@ class HalfBigNet(torch.nn.Module):
         def __init__(self, channels):
             super().__init__()
             self.model = torch.nn.Sequential(
-                torch.nn.HalfLinear(channels, channels),
+                HalfLinear(channels, channels),
                 torch.nn.ReLU(),
-                torch.nn.HalfLinear(channels, channels),
+                HalfLinear(channels, channels),
                 torch.nn.ReLU(),
-                torch.nn.HalfLinear(channels, channels),
+                HalfLinear(channels, channels),
             )
 
         def forward(self, x: torch.Tensor):
