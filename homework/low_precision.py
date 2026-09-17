Fixed low precision buffer and backward gradient issue

homework/low_precision.py
@@ -76,7 +76,10 @@ class Linear4Bit(torch.nn.Module):
             weight = state_dict[f"{prefix}weight"]  # noqa: F841
             del state_dict[f"{prefix}weight"]
             # TODO: Quantize the weights and store them in self.weight_q4 and self.weight_norm
-            self.weight_q4, self.weight_norm = block_quantize_4bit(torch.flatten(weight))
+            weight_q4, weight_norm = block_quantize_4bit(torch.flatten(weight))
+
+            self.weight_q4.copy_(weight_q4)
+            self.weight_norm.copy_(weight_norm)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         with torch.no_grad():